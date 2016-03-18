92916ac0d89164a24dbb58b328e6b04e85cacaf7
Michael Gerten <mcgerten@knox.edu>
2016-03-17 19:56:17 -0500

    wooooo

--- Robot/src/org/usfirst/frc4212/Robot/OI.java
+++ Robot/src/org/usfirst/frc4212/Robot/OI.java
@@ -2,6 +2,7 @@ package org.usfirst.frc4212.Robot;
 
 import org.usfirst.frc4212.Robot.commands.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.*;
 
@@ -19,36 +20,65 @@ public class OI {
     public static JoystickButton jB5;
     public static JoystickButton jB6;
     public static JoystickButton jB7;
-    public static Joystick joystick1;
-
+    
+    public static JoystickButton jBX1;
+    public static JoystickButton jBX2;
+    public static JoystickButton jBX3;
+    public static JoystickButton jBX4;
+    
+    public static Joystick left;
+//	public static Joystick right;
+	public static Joystick xbox;
+	
     public OI() {
 
-        joystick1 = new Joystick(0);
+        left = new Joystick(0);
+//        right = new Joystick(1);
+        xbox = new Joystick(2);
+//        
+//        jB7 = new JoystickButton(left, 11);
+//        jB7.whenPressed(new Task1());
+//        
+        //Elevator
+//        jB1 = new JoystickButton(left, 3);
+//        jB1.whileHeld(new ExtendElev());
+//        jB1.whenReleased(new ElevStop());
+//        
+//        jB2 = new JoystickButton(left, 2);
+//        jB2.whileHeld(new RetractElev());
+//        jB2.whenReleased(new ElevStop());
         
-        jB7 = new JoystickButton(joystick1, 11);
-        jB7.whenPressed(new Task1());
+        jBX1 = new JoystickButton(xbox, 1);
+        jBX1.whileHeld(new ExtendElev());
+        jBX1.whenReleased(new ElevStop());
         
-        jB1 = new JoystickButton(joystick1, 4);
-        jB1.whileHeld(new ExtendElev());
-        jB1.whenReleased(new ElevStop());
+        jBX2 = new JoystickButton(xbox, 3);
+        jBX2.whileHeld(new RetractElev());
+        jBX2.whenReleased(new ElevStop());
         
-        jB2 = new JoystickButton(joystick1, 5);
-        jB2.whileHeld(new RetractElev());
-        jB2.whenReleased(new ElevStop());
+        //Arm
+//        jB3 = new JoystickButton(left, 4);
+//        jB3.whileHeld(new RaiseArm());
+//        jB3.whenReleased(new ArmStop());
+//        
+//        jB4 = new JoystickButton(left, 5);
+//        jB4.whileHeld(new LowerArm());
+//        jB4.whenReleased(new ArmStop());
+//        
+        jBX3 = new JoystickButton(xbox, 6);
+        jBX3.whileHeld(new RaiseArm());
+        jBX3.whenReleased(new ArmStop());
         
-        jB3 = new JoystickButton(joystick1, 1);
-        jB3.whileHeld(new RaiseArm());
-        jB3.whenReleased(new ArmStop());
-        
-        jB4 = new JoystickButton(joystick1, 2);
-        jB4.whileHeld(new LowerArm());
-        jB4.whenReleased(new ArmStop());
-                
-        jB5 = new JoystickButton(joystick1, 3);
+        jBX4 = new JoystickButton(xbox, 5);
+        jBX4.whileHeld(new LowerArm());
+        jBX4.whenReleased(new ArmStop());
+             
+        //Pickup-Shoot
+        jB5 = new JoystickButton(left, 7);
         jB5.whileHeld(new IntakePickup());
         jB5.whenReleased(new PickupStop());
         
-        jB6 = new JoystickButton(joystick1, 7);
+        jB6 = new JoystickButton(left, 6);
         jB6.whileHeld(new ShootPickup());
         jB6.whenReleased(new PickupStop());
         
@@ -66,7 +96,10 @@ public class OI {
     }
 
     public Joystick getJoystick1() {
-        return joystick1;
+        return left;
     }
+//    public Joystick getJoystick2() {
+//        return right;
+//    }
 }
 
--- Robot/src/org/usfirst/frc4212/Robot/commands/Auto1.java
+++ Robot/src/org/usfirst/frc4212/Robot/commands/Auto1.java
@@ -2,26 +2,47 @@ package org.usfirst.frc4212.Robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc4212.Robot.Robot;
+import org.usfirst.frc4212.Robot.RobotMap;
 
 /**
  *
  */
 public class Auto1 extends Command {
-
+	int x;
+	int count;
+	boolean done = false;
+	
     public Auto1() {
 
         requires(Robot.driveTrain);
-
+        requires(Robot.arm);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	x = 400;
+    	count =0;
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    		
+    		done = true;
+    		if (count < x && count < 10){
+    			Robot.arm.up(1);
+    			count++;
+    		}else if (count < x){
+    			RobotMap.driveTrainRD.drive(.5, 0);
+    			Robot.arm.stop();
+    			count++;
+    		}else{
+    			RobotMap.driveTrainRD.stopMotor();	
+    			Robot.arm.stop();
+    		}
+    	}
     	
-    }
+    	
+    
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
